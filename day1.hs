e8245f347e5b905b371cc75fd12d00d9d687ca6d
E. G. Bland <egb528@york.ac.uk>
2020-12-01 06:09:20 +0000

    change day1.hs

--- day1.hs
+++ day1.hs
@@ -4,17 +4,9 @@ prodPairs n xs = [(x,y) | x <- xs, y <- xs, x+y == n]
 prodTrips :: Int -> [Int] -> [(Int,Int,Int)]
 prodTrips n xs = [(x,y,z) | x <- xs, y <- xs, z <- xs, x+y+z == n]
 
-problem1 = do
-    txt <- readFile "day1.txt"
-    let theLines = lines txt
-    let theNums = map read theLines :: [Int]
-    (putStrLn . show . (\(x,y) -> x*y) . head . (prodPairs 2020)) theNums
+problem1 = readFile "day1.txt" >>= (putStrLn . show . (\(x,y) -> x*y) . head . (prodPairs 2020) . (map read) . lines)
 
-problem2 = do
-    txt <- readFile "day1.txt"
-    let theLines = lines txt
-    let theNums = map read theLines :: [Int]
-    (putStrLn . show . (\(x,y,z) -> x*y*z) . head . (prodTrips 2020)) theNums
+problem2 = readFile "day1.txt" >>= (putStrLn . show . (\(x,y,z) -> x*y*z) . head . (prodTrips 2020) . (map read) . lines)
 
 main = do
     problem1
